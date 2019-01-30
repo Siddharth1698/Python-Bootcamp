57cccb63c4d6be53c24bb9d23641f282a8c156b8
Siddharth M <siddharth.m98@gmail.com>
2019-01-31 00:48:12 +0530

    check-number-is-positive-negative-or-zero

--- Reverse the number.py
+++ Reverse the number.py
@@ -3,7 +3,7 @@ rev = 0
 while(a>0):
     dig = a % 10
     rev = rev*10 + dig
-    a = a // 10  
+    a = a // 10
 
 print(rev)
 
---
+++ check-number-is-positive-negative-or-zero.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number"))
+
+if num > 0:
+    print("{0} is a positive number".format(num))
+
+elif num < 0:
+    print("{0} is a negetive number".format(num))
+
+else:
+    print("{0} is zero".format(num))
